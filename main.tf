e35a69fbd53f1490b089516b351f6887bd34e05d
SUPUN KAVINDA <naskavinda@gmail.com>
2023-04-17 16:12:46 +0000

    Add VPC resources block

--- main.tf
+++ main.tf
@@ -4,6 +4,14 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-06e46074ae430fba6"
   instance_type = "t2.micro"
