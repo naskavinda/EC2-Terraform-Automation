8020c61748a1bfd2d78f174cd3e901b8dabc0cf1
SUPUN KAVINDA <naskavinda@gmail.com>
2023-04-22 19:05:49 +0000

    add security group

--- main.tf
+++ main.tf
@@ -12,9 +12,36 @@ resource "aws_vpc" "example" {
   }
 }
 
+resource "aws_security_group" "example" {
+  name_prefix = "example-sg-"
+  vpc_id      = aws_vpc.example.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
   ami                    = "ami-06e46074ae430fba6"
   instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.example.id]
   tags = {
     Name        = "Dev-056"
     Environment = "Dev"
